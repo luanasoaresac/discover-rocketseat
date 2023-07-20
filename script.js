c41b1740d82e944b69139cdd7a2e54caf0f9975a
Luana Soares <luanasoaresac@gmail.com>
2023-07-19 21:12:23 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // if(html.classList.remove('light')) {
+  //else{
+  //  html.classList.add('light')
+  // }
+  //}
+  
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  if(html.classList.contains('light')) {
+     //substituir a imagem
+    img.setAttribute('src', './assets/avatar-light.png')
+    //se tiver light mode, adicionar a imagem light
+  } else{
+      //se tiver sem lighr mode, manter a imagem normal
+  img.setAttribute('src','./assets/avatar.png')
+}
+
+
+
+
+}
