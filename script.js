14d3d28a05bcacdab4d91688c59d6a3b8c625b29
Luana Soares <luanasoaresac@gmail.com>
2023-07-19 23:03:45 -0300

    Change alt of images

--- script.js
+++ script.js
@@ -13,10 +13,17 @@ function toggleMode() {
   if(html.classList.contains('light')) {
      //substituir a imagem
     img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute(
+      "alt", "Foto de perfil da Luana Soares, com óculos de grau, sorrindo e usando uma blusa azul"
+      )
     //se tiver light mode, adicionar a imagem light
   } else{
       //se tiver sem lighr mode, manter a imagem normal
   img.setAttribute('src','./assets/avatar.png')
+     img.setAttribute(
+       "alt",
+       "Foto de perfil da Luana Soares, em preto e branco, com rabiscos, onde se transforma em palhaço"
+     )
 }
 
 
